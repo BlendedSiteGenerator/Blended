1a755a6c0e0709dcce977f5be15815ff1d7935a7
John Roper <johnroper100@gmail.com>
2016-12-03 18:44:44 -0500

    bumped the version to 1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='blended',
-      version='1.2',
+      version='1.3',
       description='Static Site Generation Using HTML and Python',
       url='https://github.com/johnroper100/Blended',
       author='John Roper',
