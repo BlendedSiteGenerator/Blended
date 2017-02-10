9fc74e639cead5ea646ae359cb00664f0dc676a3
John Roper <johnroper100@gmail.com>
2017-02-10 15:06:36 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='blended',
-      version='4.0',
+      version='4.1',
       description='The Most Versatile Static HTML Site Generator',
       url='http://jmroper.com/blended/',
       author='John Roper',
