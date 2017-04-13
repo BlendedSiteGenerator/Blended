15e49756bbe21b06a2cfc8e5f75e7fb99b78aa52
John Roper <johnroper100@gmail.com>
2017-04-12 20:17:16 -0400

    starting more work on web ui

--- blended/web_ui.py
+++ blended/web_ui.py
@@ -1,6 +1,9 @@
 from flask import Flask
 web_app = Flask(__name__)
 
+# Very important, get the directory that the user wants to run commands in
+cwd = os.getcwd()
+
 @web_app.route("/")
 def hello():
     return "Hello World!"
