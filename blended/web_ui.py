e7eb97f022cacab9d80ffc429174e829e56e8fed
John Roper <johnroper100@gmail.com>
2017-04-13 07:26:08 -0400

    Added all the views

--- blended/web_ui.py
+++ blended/web_ui.py
@@ -5,14 +5,47 @@ web_app = Flask(__name__, template_folder="web_templates")
 # Very important, get the directory that the user wants to run commands in
 cwd = os.getcwd()
 
+
 @web_app.route("/")
-def hello():
-    return "Hello World! The cwd is: %s" % cwd
+def dashboard():
+    published_files = 0
+    unpublished_files = 0
+    for root, dirs, files in os.walk(os.path.join(cwd, "content")):
+        dirs[:] = [d for d in dirs if "_" not in d]
+        for filename in files:
+            if not filename.startswith("_"):
+                published_files = published_files + 1
+            else:
+                unpublished_files = unpublished_files + 1
+
+    return render_template('dashboard.html', published=published_files, unpublished=unpublished_files)
+
+
+@web_app.route("/files/")
+def files():
+    published_files = 0
+    unpublished_files = 0
+    for root, dirs, files in os.walk(os.path.join(cwd, "content")):
+        dirs[:] = [d for d in dirs if "_" not in d]
+        for filename in files:
+            if not filename.startswith("_"):
+                published_files = published_files + 1
+            else:
+                unpublished_files = unpublished_files + 1
+
+    return render_template('files.html', published=published_files, unpublished=unpublished_files)
+
 
 @web_app.route("/edit/<path:filename>")
 def edit_file(filename):
     content = open(os.path.join(cwd, "content", filename), 'r').read()
     return render_template('edit.html', content=content, filename=filename)
+
+
+@web_app.route("/edit/")
+def edit_redirect():
+    return redirect("/files/", code=302)
+
 
 @web_app.route("/publish/", methods=['POST'])
 def publish():
@@ -22,4 +55,4 @@ def publish():
     with open(os.path.join(cwd, "content", filename), 'w') as wfile:
         wfile.write(content)
 
-    return redirect("/edit/"+filename+"?finished=yes", code=302)
+    return redirect("/edit/" + filename + "?finished=yes", code=302)
