35c46aba86a6a16e2edf3b46ed1eebe895109d37
John Roper <johnroper100@gmail.com>
2017-05-05 07:40:09 -0400

    Started v6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='blended',
-      version='5.0.2',
+      version='6.0.0',
       description='The Most Versatile Static HTML Site Generator',
       url='http://jmroper.com/blended/',
       author='John Roper',
@@ -11,26 +11,11 @@ setup(name='blended',
       include_package_data=True,
       install_requires=[
           'click',
-          'colorama',
-          'watchdog',
-          'Markdown',
-          'textile',
-          'docutils',
-          'mammoth',
-          'libsass',
-          'pyjade',
-          'lesscpy',
-          'stylus',
-          'coffeescript',
-          'jsmin',
-          'cssmin',
-          'untangle',
           'python-frontmatter',
-          'Flask',
       ],
       entry_points={
           'console_scripts': [
-              'blended=blended.__main__:cli',
+              'blended=blended.__init__:cli',
           ],
       },
       zip_safe=False)
