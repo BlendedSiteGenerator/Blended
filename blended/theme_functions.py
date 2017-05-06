7c2b7864d693711463e0f1eaf12bf0735c55b3ac
John Roper <johnroper100@gmail.com>
2017-05-06 15:08:58 -0400

    Finished setup theme

--- blended/theme_functions.py
+++ blended/theme_functions.py
@@ -26,5 +26,6 @@ def setupTheme(theme):
 
     config['theme_params'] = theme_config
 
-    print(json.dumps(OrderedDict([("blended_version", config['blended_version']), ("title", config['title']), ("subtitle", config['subtitle']), ("description", config['description']), ("language", config['language']), ("theme", config['theme']),
-                                  ("build_home", config['build_home']), ("build_posts", config['build_home']), ("build_pages", config['build_pages']), ("build_authors", config['build_authors']), ("theme_params", config['theme_params'])]), indent=4))
+    with open(config_file_dir, 'w') as config_file:
+        config_file.write(json.dumps(OrderedDict([("blended_version", config['blended_version']), ("title", config['title']), ("subtitle", config['subtitle']), ("description", config['description']), ("language", config['language']),
+                                                  ("theme", config['theme']), ("build_home", config['build_home']), ("build_posts", config['build_home']), ("build_pages", config['build_pages']), ("build_authors", config['build_authors']), ("theme_params", config['theme_params'])]), indent=4))
